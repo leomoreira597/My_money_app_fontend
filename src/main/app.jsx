b747606f1988332250763a657c6f19b3a4458e32
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-08 18:57:04 -0300

    inclusão de dependencias e componente header

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import "../common/template/dependencies"
+import React from "react";
+import Header from "../common/template/header";
+
+export default function(props){
+    return(
+        <div className="wraper">
+            <Header/>
+        </div>
+    );
+}
