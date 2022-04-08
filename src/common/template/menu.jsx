98dcfa32685fa1a7d967be78cd506d2fa5e2c2ad
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-08 19:07:57 -0300

    Adicionando side bar

---
+++ src/common/template/menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function(props){
+    return(
+        <ul className="sidebar-menu">
+            
+        </ul>
+    );
+}
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,11 +1,13 @@
 import "../common/template/dependencies"
 import React from "react";
 import Header from "../common/template/header";
+import Sidebar from "../common/template/sidebar";
 
 export default function(props){
     return(
         <div className="wraper">
             <Header/>
+            <Sidebar/>
         </div>
     );
 }
