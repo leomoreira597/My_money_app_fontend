2dc12a9f4ea5595fe0537c1c4e4b38c4599b09d9
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-09 00:33:52 -0300

    criando componente de grid

---
+++ src/common/template/content.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function(props){
+    return(
+        <section className="content">
+            {props.children}
+        </section>
+    );
+}
--- src/dashBoard/dashBoard.jsx
+++ src/dashBoard/dashBoard.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
 
-export default function(props){
-    return(
-        <div>
-            <h1>
-                DashBoard
-            </h1>
-        </div>
-    );
+class DashBoard extends Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title="DashBoard" small="Versão 1.0"/>
+                <Content>
+                    DashBoard
+                </Content>
+            </div>
+        );
+    }
 }
+
+export default DashBoard
