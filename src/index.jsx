76dc49fae479f104070715e648ecee208a88c6b0
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-09 13:17:00 -0300

    Codigo do dev tools adicionado

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import { Provider } from "react-redux";
 import reducers from "./main/reducers";
 import App from "./main/app";
 
-const store =applyMiddleware(promise)(createStore)(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store =applyMiddleware(promise)(createStore)(reducers, devTools)
 ReactDom.render(
     <Provider store={store}>
         <App/> 
