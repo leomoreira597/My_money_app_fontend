1d6144e5e8a662fe54019b694cbd03e9aac7f748
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-11 12:52:27 -0300

    Actions tab

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import DashBoardReducer from "../dashBoard/dashBoardReducer";
+import TabReducer from "../common/tab/tabReducer";
 
 const rootReducer = combineReducers({
-    dashBoard: DashBoardReducer
+    dashBoard: DashBoardReducer,
+    tab: TabReducer
 })
 
 export default rootReducer
