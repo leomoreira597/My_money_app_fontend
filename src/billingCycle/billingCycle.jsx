8e61fdb38083fd430e97e2e9fbf205b7e7c8abce
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-11 12:03:35 -0300

    Criando tela de ciclo de pagamentos

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {Component} from "react";
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
 
-export default function(props){
-    return(
+class BillingCycle extends Component{
+    render(){
         <div>
-            <h1>
-                Ciclo de Pagamentos
-            </h1>
+            <ContentHeader title="Ciclo de Pagamentos" small="Cadastro"/>
+            <Content>
+                Ciclos de pagamentos
+            </Content>
         </div>
-    );
+    }
 }
+
+export default BillingCycle
