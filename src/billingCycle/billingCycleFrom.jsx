b1a56e4be012f298e2af2945156dc0141607a601
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-12 15:21:58 -0300

    front end finalizado

--- src/billingCycle/billingCycleFrom.jsx
+++ src/billingCycle/billingCycleFrom.jsx
@@ -5,10 +5,22 @@ import { init } from "./billingCyclesActions";
 import {reduxForm, Field, formValueSelector } from "redux-form";
 import labelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component{
+
+    calculateSummary(){
+        const sum = (t, v) => t + v
+        return{
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+
+        }
+    }
+
     render(){
-        const {handleSubmit, readOnly, credits} = this.props
+        const {handleSubmit, readOnly, credits, debts} = this.props
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary()
         return(
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -23,8 +35,13 @@ class BillingCycleForm extends Component{
                         readOnly={readOnly} type="number"
                         label="Ano" cols="12 4" placeholder="Informe o ano"/> 
 
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />    
+
                     <ItemList cols="12 6" list={credits} readOnly={readOnly} 
                         field='credits' legend="Créditos"/>
+
+                    <ItemList cols="12 6" list={debts} readOnly={readOnly} 
+                        field='debts' legend="Débitos" showStatus={true}/>    
 
                 </div>
                 <div className="box-footer">
@@ -47,7 +64,8 @@ const selector = formValueSelector('billingCycleForm')
 
 function mapStateToProps(state){
     return{
-        credits: selector(state, 'credits')
+        credits: selector(state, 'credits'),
+        debts: selector(state, 'debts')
     }
 }
 
--- src/billingCycle/itemList.jsx
+++ src/billingCycle/itemList.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from "redux";
 import { Field, arrayInsert, arrayRemove } from "redux-form";
 import Grid from "../common/layout/grid";
 import Input from "../common/form/input";
+import If from "../common/operator/if";
 
 class ItemList extends Component{
 
@@ -26,8 +27,13 @@ class ItemList extends Component{
                 <td><Field name={`${this.props.field}[${index}].name`} component={Input} 
                     placeholder="Informe o nome" readOnly={this.props.readOnly}/></td>
 
-                <td><Field name={`${this.props.field}[${index}].name`} component={Input}
+                <td><Field name={`${this.props.field}[${index}].value`} component={Input}
                     placeholder="Informe o valor" readOnly={this.props.readOnly}/></td>
+
+                <If test={this.props.showStatus}>
+                    <td><Field name={`${this.props.field}[${index}].status`} component={Input}
+                        placeholder="Informe o status" readOnly={this.props.readOnly}/></td>
+                </If>    
 
                 <td>
                     <button type="button" className="btn btn-success"
@@ -60,6 +66,9 @@ class ItemList extends Component{
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className="table-actions">Ações</th>
                             </tr>
                         </thead>
