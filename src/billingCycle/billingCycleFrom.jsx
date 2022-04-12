8b6e027f1ca110044845a220e995cedb80195d0b
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-12 12:51:00 -0300

    primeira versão da lista de creditos

--- src/billingCycle/billingCycleFrom.jsx
+++ src/billingCycle/billingCycleFrom.jsx
@@ -4,6 +4,7 @@ import { bindActionCreators } from "redux";
 import { init } from "./billingCyclesActions";
 import {reduxForm, Field } from "redux-form";
 import labelAndInput from "../common/form/labelAndInput";
+import CrediList from "./crediList";
 
 class BillingCycleForm extends Component{
     render(){
@@ -21,6 +22,9 @@ class BillingCycleForm extends Component{
                     <Field name="year" component={labelAndInput} 
                         readOnly={readOnly} type="number"
                         label="Ano" cols="12 4" placeholder="Informe o ano"/> 
+
+                    <CrediList cols="12 6" />
+
                 </div>
                 <div className="box-footer">
                     <button type="submit" className={`btn btn-${this.props.submitClass}`}>
