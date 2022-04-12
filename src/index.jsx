61099191f1543c82c2cbad59eb0d75bc5f29e7e9
Leonardo Moreira <lmoreira597@gmail.com>
2022-04-11 23:05:04 -0300

    Limpeza de formulario

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,12 @@ import promise from "redux-promise";
 import { Provider } from "react-redux";
 import reducers from "./main/reducers";
 import App from "./main/app";
+import multi from "redux-multi";
+import thunk from "redux-thunk"
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store =applyMiddleware(promise)(createStore)(reducers, devTools)
+const store =applyMiddleware(multi, thunk ,promise)(createStore)(reducers, devTools)
 ReactDom.render(
     <Provider store={store}>
         <App/> 
